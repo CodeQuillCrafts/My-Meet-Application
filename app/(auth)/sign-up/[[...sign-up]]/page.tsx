7e5a512d5e72c054ee36b1b66af6d708aedb15e1
Soumay-Sanpui <somusanpui9@gmail.com>
2024-04-14 11:17:21 +0530

    initial commit

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from "@clerk/nextjs"
+const SignUpPage = () => {
+  return (
+    <main className="h-screen w-full flex-center">
+        <SignUp />
+    </main>
+  )
+}
+
+export default SignUpPage 
